e37679b07353b35242e1a175436d2885edfac305
ArchanaBalaji11 <141020158+ArchanaBalaji11@users.noreply.github.com>
2023-08-09 14:17:29 +0530

    Add files via upload

---
+++ Collection.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Collection
+ {
+  public static void main(String[] args) 
+  {
+  ArrayList<String> list_Strings = new ArrayList<String>();
+  list_Strings.add("Red");
+  list_Strings.add("Green");
+  list_Strings.add(" ");
+  list_Strings.add("White");
+  list_Strings.add("Black");
+  System.out.println(list_Strings);
+ }
+} 
